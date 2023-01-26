9602c3bc39c32a90c43d09419027b8eddeb95cdd
Daniel Mateu <danielmateu86@gmail.com>
2023-01-26 19:06:28 +0100

    componente acumular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // template: `
+  // <h1>Hola mundo!</h1>
+  // `
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  title : string = 'Contador App';
+  numero: number = 10;
+  base  : number = 100
+
+
+  acumular(valor: number){
+    this.numero += valor;
+  }
+
+
+  // sumar(){
+  //   this.numero += 1;
+  // }
+
+  // restar(){
+  //   this.numero -= 1;
+  // }
 }
