35510209a4d7363dec03f95bde3fbf6557d3e9c8
Daniel Mateu <danielmateu86@gmail.com>
2023-01-26 19:29:27 +0100

    Creaciopn manual de componente heroe

---
+++ src/app/heroes/heroe/heroe.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-heroe',
+  // templateUrl: './heroe.component.html',
+  template: `
+  <h1>From Heroe Component</h1>
+  `
+})
+
+export class HeroeComponent {
+
+}
