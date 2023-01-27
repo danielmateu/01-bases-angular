d1c2b607ae55f806dfcfc867ca5379fcdbb28557
Daniel Mateu <danielmateu86@gmail.com>
2023-01-27 13:38:58 +0100

    Creación de componentes hijos

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -13,14 +13,33 @@ interface Personaje {
 
 export class MainPageComponent {
 
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 8500
+    }
+  ];
+
   nuevo: Personaje = {
-    nombre: 'Trunks',
-    poder: 12500
+    nombre: '',
+    poder: 0
   }
 
   agregar(){
-    // e.preventDefault();
-    console.log(this.nuevo)
+    if(this.nuevo.nombre.trim().length === 0) return;
+
+    this.personajes.push(this.nuevo)
+
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
+
+
   }
 
 }
