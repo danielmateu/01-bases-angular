880790e9ababbd7f5f6f5338ec1802d68c5fbf39
Daniel Mateu <danielmateu86@gmail.com>
2023-01-27 09:33:00 +0100

    Cambios en template del componente

--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -2,12 +2,21 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-heroe',
-  // templateUrl: './heroe.component.html',
-  template: `
-  <h1>From Heroe Component</h1>
-  `
+  templateUrl: './heroe.component.html',
+  // template: `
+  // <h1>From Heroe Component</h1>
+  // `
 })
 
 export class HeroeComponent {
+  nombre: string = 'Iron Man'
+  edad: number = 37
 
+  get nombreCapitalizado() {
+    return this.nombre.toUpperCase();
+  }
+
+  obtenerNombre(): string{
+    return `Nombre: ${this.nombre} - Edad:${this.edad}`
+  }
 }
